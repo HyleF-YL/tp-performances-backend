7257e06480d09762869ead098677adf9afbb49d7
HyleF-YL <97347658+HyleF-YL@users.noreply.github.com>
2022-12-11 23:46:13 +0100

    question 5

--- src/Services/Hotel/OneRequestHotelService.php
+++ src/Services/Hotel/OneRequestHotelService.php
@@ -1,1 +1,35 @@
 <?php
+    namespace App\Services\Hotel;
+
+    use App\Common\SingletonDB;
+    use App\Common\Timers;
+    use App\Entities\HotelEntity;
+    use App\Services\Room\AbstractRoomService;
+    use PDO;
+    use PDOStatement;
+
+    class OneRequestHotelService extends AbstractHotelService {
+
+        protected function getDB (): PDO
+        {
+            $timer = Timers::getInstance();
+            $timerID = $timer->startTimer('getDB');
+            $pdo = SingletonDB::get();
+            $timer->endTimer('getDB',$timerID);
+            return $pdo;
+        }
+
+        public function list(array $args = []): array
+        {
+            // TODO: Implement list() method.
+        }
+
+        /**
+         * @param array $args
+         * @return PDOStatement
+         */
+
+        protected function buildQuery(array $args) : PDOStatement {
+
+        }
+    }
